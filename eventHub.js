cbebfee905d0171da1da922084969c85995ac73d
lc <1033581609@qq.com>
2020-10-02 16:52:36 +0800

    on

--- eventHub.js
+++ eventHub.js
@@ -4,7 +4,16 @@ class EventHub {
             eventName: [fn1, fn2, fn3, ...]
         */
     };
-    on(eventName, fn) {}
-    emit(eventName) {}
-    off(eventName, fn) {}
+    on(eventName, fn) {
+        this.events[eventName] == null ? (this.events[eventName] = []) : null;
+        this.events[eventName].push(fn);
+    }
+    emit(eventName) {
+    }
+    off(eventName, fn) {
+    }
 }
+
+module.exports = {
+    EventHub,
+};
